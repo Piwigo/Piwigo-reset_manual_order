a5340c01c487a09b1187edf95ee8960561473c06
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-06-14 06:27:41 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,8 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Reset manual order with current automatic order'] = 'Endurstilla handvirka röð með núverandi sjálfvirkri röð';
